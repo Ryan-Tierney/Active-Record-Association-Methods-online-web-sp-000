eeb32102f431d141d9666c0f2dfaa851f081080d
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-27 18:30:28 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,9 +1,13 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+  
+  def get_first_song 
+    self.songs.first 
+  end 
 
   def get_genre_of_first_song
-    self.songs.genres.first 
+    get.first.song.genre
   end
 
   def song_count
